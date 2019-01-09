33085de98ce5238e1f692d391ca560755b62225f
EngTaian <taian.silva@hotmail.com>
2019-01-09 11:29:38 -0200

    Primeiro commit

---
+++ src/main/java/com/taian/cursospringbootcomionic/CursospringbootcomionicApplication.java
@@ -0,0 +1,14 @@
+package com.taian.cursospringbootcomionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringbootcomionicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringbootcomionicApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/taian/cursospringbootcomionic/CursospringbootcomionicApplicationTests.java
@@ -0,0 +1,17 @@
+package com.taian.cursospringbootcomionic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CursospringbootcomionicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
