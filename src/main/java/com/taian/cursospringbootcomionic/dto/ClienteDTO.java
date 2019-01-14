edb26d67456ee4d53d2e1f9dd69f13d0754d02c4
EngTaian <taian.silva@hotmail.com>
2019-01-14 16:58:15 -0200

    Validação de e-mail e verificação de duplicidade

--- src/main/java/com/taian/cursospringbootcomionic/dto/ClienteDTO.java
+++ src/main/java/com/taian/cursospringbootcomionic/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.taian.cursospringbootcomionic.domain.Cliente;
+import com.taian.cursospringbootcomionic.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
---
+++ src/main/java/com/taian/cursospringbootcomionic/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,50 @@
+package com.taian.cursospringbootcomionic.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.taian.cursospringbootcomionic.domain.Cliente;
+import com.taian.cursospringbootcomionic.dto.ClienteDTO;
+import com.taian.cursospringbootcomionic.repositories.ClienteRepository;
+import com.taian.cursospringbootcomionic.resources.exception.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer id = Integer.parseInt(map.get("id"));
+		
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		if (aux != null && !aux.getId().equals(id)) {
+			list.add(new FieldMessage("email", "E-mail já existe na base de dados"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
