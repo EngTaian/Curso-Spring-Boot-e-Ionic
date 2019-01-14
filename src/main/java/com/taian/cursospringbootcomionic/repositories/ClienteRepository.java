f41fcf9a5ef7fcab4a4e7f6947c732286d1d7711
EngTaian <taian.silva@hotmail.com>
2019-01-14 16:47:13 -0200

    Validacao customizada de e-mail do cliente

--- src/main/java/com/taian/cursospringbootcomionic/repositories/ClienteRepository.java
+++ src/main/java/com/taian/cursospringbootcomionic/repositories/ClienteRepository.java
@@ -2,13 +2,13 @@ package com.taian.cursospringbootcomionic.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.taian.cursospringbootcomionic.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 	
-		
-	
-
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/taian/cursospringbootcomionic/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/taian/cursospringbootcomionic/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.taian.cursospringbootcomionic.domain.Cliente;
 import com.taian.cursospringbootcomionic.domain.enums.TipoCliente;
 import com.taian.cursospringbootcomionic.dto.ClienteNewDTO;
+import com.taian.cursospringbootcomionic.repositories.ClienteRepository;
 import com.taian.cursospringbootcomionic.resources.exception.FieldMessage;
 import com.taian.cursospringbootcomionic.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -26,6 +34,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && ! BR.isValidTin(objDto.getCpfCnpj())) {
 			list.add(new FieldMessage("cpfCnpj", "Cnpj Inválido"));
+		}
+		
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "E-mail já existe"));
 		}
 		
 		for (FieldMessage e : list) {
