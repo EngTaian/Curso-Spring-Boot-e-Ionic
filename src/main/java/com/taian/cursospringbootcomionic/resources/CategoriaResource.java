9e4b84dd2275855ac48162518ca3736be3cc632d
EngTaian <taian.silva@hotmail.com>
2019-01-14 10:49:49 -0200

    Atualizando categoria com PUT

--- src/main/java/com/taian/cursospringbootcomionic/resources/CategoriaResource.java
+++ src/main/java/com/taian/cursospringbootcomionic/resources/CategoriaResource.java
@@ -29,9 +29,9 @@ public class CategoriaResource {
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> findById(@PathVariable Integer id) {
+	public ResponseEntity<Categoria> findById(@PathVariable Integer id) {
 		
-		Categoria obj = service.buscar(id);
+		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 
 	}
@@ -43,6 +43,11 @@ public class CategoriaResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
-	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
 
 }
--- src/main/java/com/taian/cursospringbootcomionic/services/CategoriaService.java
+++ src/main/java/com/taian/cursospringbootcomionic/services/CategoriaService.java
@@ -22,7 +22,7 @@ public class CategoriaService {
 		return obj;
 	}
 
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! id: " + id + ", Tipo: " + Categoria.class.getName()));
@@ -33,5 +33,9 @@ public class CategoriaService {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
 }
