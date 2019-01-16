31566e14d9bfa66c2bf2340b77a1a5114d44fcec
EngTaian <taian.silva@hotmail.com>
2019-01-16 16:49:23 -0200

    Configuração e envio do projeto para produção

---
+++ src/main/java/com/taian/cursospringbootcomionic/config/DbProdConfiguration.java
@@ -0,0 +1,34 @@
+package com.taian.cursospringbootcomionic.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.taian.cursospringbootcomionic.services.DbService;
+
+@Configuration
+@Profile("prod")
+public class DbProdConfiguration {
+	
+	@Autowired
+	private DbService dbService;
+	
+	@Value("{spring.jpa.hibernate.ddl-auto}")
+	private String db;
+	
+	@Bean
+	public boolean dbTest() throws ParseException {
+		
+		if(!"create".equals(db)) {
+			return false;
+		}
+		dbService.instantiateDbServerTest();
+		return true;
+		
+	} 
+	
+}
