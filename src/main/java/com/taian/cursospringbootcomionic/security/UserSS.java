9092a270a58c7ebee503119a6ee4f8fd1f9aeee7
EngTaian <taian.silva@hotmail.com>
2019-01-21 17:38:47 -0200

    Implementando autenticação e geração do token JWT

---
+++ src/main/java/com/taian/cursospringbootcomionic/security/UserSS.java
@@ -0,0 +1,90 @@
+package com.taian.cursospringbootcomionic.security;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.taian.cursospringbootcomionic.domain.enums.Perfil;
+
+public class UserSS implements UserDetails {
+	
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String email;
+	private String senha;
+	private Collection<? extends GrantedAuthority> authorities;
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	
+	
+	
+	public UserSS() {
+		
+	}
+	
+	public UserSS(Integer id, String email, String senha, Set<Perfil> perfils) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.senha = senha;
+		this.authorities = perfils.stream().map(x-> new SimpleGrantedAuthority(x.getDescricao())).collect(Collectors.toList());
+	}
+
+
+
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+	
+}
---
+++ src/main/java/com/taian/cursospringbootcomionic/services/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.taian.cursospringbootcomionic.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.taian.cursospringbootcomionic.domain.Cliente;
+import com.taian.cursospringbootcomionic.repositories.ClienteRepository;
+import com.taian.cursospringbootcomionic.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = clienteRepository.findByEmail(email);
+		
+		if(cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+
+	
+}
